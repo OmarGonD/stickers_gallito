956866b6f68bb168315df4b43e29c49c919a697c
ogonzales <oma.gonzales@gmail.com>
2019-03-18 16:14:46 -0500

    Fixing manage.py and dotenv

--- manage.py
+++ manage.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+
 import dotenv
 
 if __name__ == '__main__':
-    dotenv.read_dotenv()
+    dotenv.read_dotenv(override=True)
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stickers_gallito.settings')
     try:
         from django.core.management import execute_from_command_line
