c72d095e1af50488595e65e53a7529e6598aed5c
Stefan Georg <github@stefangeorg.net>
2023-09-30 08:14:34 +0200

    Removed manifest static file storage

--- stickers_gallito/settings.py
+++ stickers_gallito/settings.py
@@ -201,7 +201,7 @@ STORAGES = {
     },
 
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
